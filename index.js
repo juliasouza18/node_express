c275a03792612d34cb9cd179823e5024cd1f3b6b
EA - Julia Souza Costa <julia_costa01@educadventista.org>
2023-11-10 10:19:13 -0300

    final video-2

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const path = require("path")
 const app = express()
 
 const caminhoBase= path.join(__dirname, "templates") 
+
+// htps://localhost:3000/usuarios/20
+app.get('/usuarios/:id', (requisicao, resposta) => {
+    const id= requisicao.params.id
+
+    console.log(`acessando dados do usuário ${id}`)
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
+
+// htps://localhost:3000
 app.get('/', (requisicao, resposta) => {
     resposta.sendFile(`${caminhoBase}/index.html`)
 })
