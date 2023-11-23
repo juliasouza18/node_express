2e9a3d5f203e122543af5683f671587b624ed8fe
EA - Julia Souza Costa <julia_costa01@educadventista.org>
2023-11-23 07:45:40 -0300

    recebendo informações do formulário no corpo da requisição

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ const app = express()
 
 const caminhoBase= path.join(__dirname, "templates") 
 
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+
+  
+app.post('/cadastrar/salvar', (requisicao, resposta) => {
+    const nome= requisicao.body.nomeCompleto
+    const email= requisicao.body.emailTodo
+    const senha= requisicao.body.senhaTop
+
+    console.log(`O email do usuário é: ${email}`)
+})
 app.get('/cadastrar', (requisiçao, resposta)=> {
     resposta.sendFile(`${caminhoBase}/cadastro.html`)
 })
